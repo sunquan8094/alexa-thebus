b061ccd78be8420a56385166106b02e611c1639b
dchan3 <dchan3@hawaii.edu>
2017-08-12 15:27:10 -1000

    Add new intent

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TheBus Arrivals Alexa Skill
 
 This repository contains code for an AWS Lambda function that accesses the [TheBus API](http://hea.thebus.org/api_info.asp).
+
+The Lambda function cannot execute without a file called `settings.json` in the root directory. The object must contain a key called `api_key`, and the corresponding value is the TheBus API Key. Please follow the instructions in the link in the above paragraph to obtain a key.
--- index.js
+++ index.js
@@ -10,16 +10,62 @@ var handlers = {
     request({url: 'http://api.thebus.org/arrivals/?key=' + settings.api_key + '&stop=' + stop,
              method: 'GET'}, function(error, response, body) {
                var obj = parseString(body, function(err, obj) {
-                 var arr = obj.stopTimes.arrival[0];
-                 var route = arr.route[0];
-                 var estSched = arr.estimated[0] === "0" ? "scheduled" : "estimated";
-                 var time = arr.stopTime[0];
-                 self.emit(':tell', "The next bus is route " + route + ", " + estSched + " to arrive at " + time + ".");
+                 if (obj.stopTimes.arrival !== undefined) {
+                   var arr = obj.stopTimes.arrival[0];
+                   var route = arr.route[0];
+                   var estSched = arr.estimated[0] === "0" ? "scheduled" : "estimated";
+                   var time = arr.stopTime[0];
+                   self.emit(':tell', "The next bus is route " + route + ", " + estSched + " to arrive at " + time + ".");
+                 }
+                 else {
+                   self.emit(':tell', "There is no information on bus arrivals for the stop you specified at this time. Please check back later.");
+                 }
+               });
+             });
+  },
+  'GetNextRouteAtStopIntent': function() {
+    var self = this;
+    var stop = this.event.request.intent.slots.stop.value;
+    var route = this.event.request.intent.slots.route.value;
+    if (stop === undefined || route === undefined) {
+      self.emit(":ask", "I didn't get everything. Please try again.");
+      return;
+    }
+    if (/[Aa](lpha)?/.test(route)) {
+      route = 'A';
+    }
+    if (/[Cc](harlie)?/.test(route)) {
+      route = 'C';
+    }
+    if (/[Ee](echo)?/.test(route)) {
+      route = 'E';
+    }
+    request({url: 'http://api.thebus.org/arrivals/?key=' + settings.api_key + '&stop=' + stop,
+             method: 'GET'}, function(error, response, body) {
+               var obj = parseString(body, function(err, obj) {
+                 if (obj.stopTimes.arrival !== undefined) {
+                   var arrivals = obj.stopTimes.arrival;
+                   arrivals = arrivals.filter(function(arrival) {
+                     return arrival.route[0] === route;
+                   });
+                   if (arrivals.length > 0) {
+                     var res = arrivals[0];
+                     var estSched = res.estimated[0] === "0" ? "scheduled" : "estimated";
+                     var time = res.stopTime[0];
+                     self.emit(':tell', "The next route " + route + " bus is " + estSched + " to arrive at stop number " + stop + " at " + time + ".");
+                   }
+                   else {
+                     self.emit(':tell', "There is no information on route " + route + " bus arrivals " + " at stop " + stop + " at this time. Please check back later.");
+                   }
+                 }
+                 else {
+                   self.emit(':tell', "There is no information on route " + route + " bus arrivals " + " at stop " + stop + " at this time. Please check back later.");
+                 }
                });
              });
   },
   "Unhandled": function () {
-    this.emit(":tell", "I don't understand that.");
+    this.emit(":ask", "I don't understand that. Please try again.");
   },
   "LaunchRequest": function() {
     this.emit(":ask", "Aloha from TheBus. What would you like to ask?");
